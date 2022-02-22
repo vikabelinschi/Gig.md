5339e9fff60675c64a8de55a88fb26f7ae074a2b
Victoria Belinschi <victoria.belinschi@endava.com>
2022-02-22 10:31:51 +0200

    Initial Commit

---
+++ Gig.md/Gig_mdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gig_mdApp.swift
+//  Gig.md
+//
+//  Created by Victoria Belinschi on 22.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Gig_mdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
